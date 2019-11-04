a46d23bd0f70e592a7dbdd4954e11c6df3ff3442
杨朋 <yangpeng2@rlair.net>
2019-11-04 15:26:55 +0800

    升级为swift 4.2

--- Sources/Controllers/BarcodeScannerViewController.swift
+++ Sources/Controllers/BarcodeScannerViewController.swift
@@ -101,8 +101,7 @@ open class BarcodeScannerViewController: UIViewController {
     cameraViewController.metadata = metadata
     cameraViewController.delegate = self
     add(childViewController: cameraViewController)
-
-    view.bringSubview(toFront: messageView)
+    view.bringSubviewToFront(messageView)
   }
 
   open override func viewWillAppear(_ animated: Bool) {
@@ -202,8 +201,7 @@ open class BarcodeScannerViewController: UIViewController {
     flashView.alpha = 1
 
     view.addSubview(flashView)
-    view.bringSubview(toFront: flashView)
-
+    view.bringSubviewToFront(flashView)
     UIView.animate(
       withDuration: 0.2,
       animations: ({
@@ -303,7 +301,7 @@ extension BarcodeScannerViewController: CameraViewControllerDelegate {
 
   func cameraViewControllerDidTapSettingsButton(_ controller: CameraViewController) {
     DispatchQueue.main.async {
-      if let settingsURL = URL(string: UIApplicationOpenSettingsURLString) {
+        if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
         UIApplication.shared.openURL(settingsURL)
       }
     }
